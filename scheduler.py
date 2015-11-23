fa7e6740b1c255bf444ee34d08dd6e1cfeaa59a5
Charles Liu <therealcharlesliu@gmail.com>
2015-11-23 11:15:54 -0500

    fix serialization issues

--- scheduler.py
+++ scheduler.py
@@ -10,15 +10,52 @@ CSP code running DFS on variables
 import domain as dom
 import datagen as dg
 import json
+import sys
 
 def write_output(domains, filename):
     with open(filename + '.json', 'w') as fp:
-        json.dump(domains, fp)
+        json.dump(map2list(domains), fp)
 
-def read_output(filename):
+def read_output(league, filename):
     with open(filename + '.json', 'r') as fp:
-        domains = json.load(fp)
+        domains = list2map(league, json.load(fp))
     return domains
+    
+def map2list(m):
+    l = []
+    for k,v in m.iteritems():
+        new_k = []
+        new_v = []
+        for t in k:
+            if type(t) is dg.org.Team:
+                new_k.append(t.name)
+            else:
+                new_k.append(t)
+        for t in v:
+            if type(t) is dg.org.Team:
+                new_v.append(t.name)
+            else:
+                new_v.append(t)
+        l.append({'key':new_k, 'value': new_v})
+    return l
+    
+def list2map(league, l):
+    m = {}
+    for kv in l:
+        new_k = []
+        new_v = []
+        for t in kv['key']:
+            if type(t) is unicode:
+                new_k.append(league.get_team(t))
+            else:
+                new_k.append(t)
+        for t in kv['value']:
+            if type(t) is unicode:
+                new_v.append(league.get_team(t))
+            else:
+                new_v.append(t)
+        m[tuple(new_k)] = new_v
+    return m
 
 class Scheduler(object):
     
@@ -26,14 +63,15 @@ class Scheduler(object):
         self.data = dg.DataGen(year)
 
     def create_schedule(self, matchups_json = False, venues_json = False, dates_json = False):
+        print "{}: Starting".format(dg.datetime.datetime.today())
         matchups = None
         if matchups_json:
-            matchups = read_output("Matchups")
+            matchups = read_output(self.data.league, "Matchups")
         else:
             initialState = dom.Matchups(None, self.data.league, self.data.game_indices)
             matchups, matchups_statesExplored = self.DFS(initialState)
             write_output(matchups, "Matchups")        
-            print "Matchups: {}".format(matchups_statesExplored)
+            print "{}: Matchups, {} states explored".format(dg.datetime.datetime.today(), matchups_statesExplored)
         
         if matchups is not None:
             #now we have our matchups which is a dict of (team, game_num) -> [opponent]
@@ -43,7 +81,7 @@ class Scheduler(object):
             #also team1 < team2 alphabetically
             venues = None
             if venues_json:
-                venues = read_output("Venues")
+                venues = read_output(self.data.league, "Venues")
             else:
                 venues_domains = {}
                 for state in matchups:
@@ -55,7 +93,7 @@ class Scheduler(object):
                 venueState = dom.Venues(venues_domains)
                 venues, venues_statesExplored = self.DFS(venueState)
                 write_output(venues, "Venues")
-                print "Venues: {}".format(venues_statesExplored)
+                print "{}: Venues, {} states explored".format(dg.datetime.datetime.today(), venues_statesExplored)
             
             if venues is not None:
                 #now we have our venues which is a dict of (team1, team2, game_num) -> [True/False]
@@ -65,7 +103,7 @@ class Scheduler(object):
                 #domains to be roughly 2*game_num +/- window dates
                 dates = None
                 if dates_json:
-                    dates = read_output("Dates")
+                    dates = read_output(self.data.league, "Dates")
                 else:
                     window = 3
                     dates_domains = {}
@@ -81,21 +119,20 @@ class Scheduler(object):
                     dateState = dom.Dates(dates_domains)
                     dates, dates_statesExplored = self.DFS(dateState)
                     write_output(dates, "Dates")
-                    print "Dates: {}".format(dates_statesExplored)
+                    print "{}: Dates, {} states explored".format(dg.datetime.datetime.today(), dates_statesExplored)
                     
-                    if dates is not None:
-                        #create a master map of everything of the form:
-                        #(team, game_num) -> (opponent, home, dateindex)
-                        #where home is true if played at team's venue
-                        sched_map = {}
-                        for state in venues:
-                            t1, t2, g_n = state
-                            home = venues[state][0]
-                            date = dates[state][0]
-                            sched_map[(t1, g_n)] = (t2, home, date)
-                            sched_map[(t2, g_n)] = (t1, not home, date)
-                        write_output(sched_map, "Schedule")
-                        return sched_map
+                if dates is not None:
+                    #create a master map of everything of the form:
+                    #(team, game_num) -> (opponent, home, dateindex)
+                    #where home is true if played at team's venue
+                    sched_map = {}
+                    for state in venues:
+                        t1, t2, g_n = state
+                        home = venues[state][0]
+                        date = dates[state][0]
+                        sched_map[(t1, g_n)] = (t2, home, date)
+                        sched_map[(t2, g_n)] = (t1, not home, date)
+                    return sched_map
                     
         return None
         
@@ -106,6 +143,7 @@ class Scheduler(object):
         while frontier:
             state = frontier.pop()
             statesExplored += 1
+            print "{}, {}".format(statesExplored, len(frontier))
             successors = state.successors()
             if successors is None:
                 return (state.domains, statesExplored)
@@ -120,10 +158,10 @@ class Scheduler(object):
         if type(teams) is str:
             teams = [teams]
         for t in (teams if len(teams) > 0 else self.data.league.teams()):
-            team = self.data.league.get_team(t)
+            t = self.data.league.get_team(t) if type(t) is str else t
             for i in range(1, games + 1):
-                dom_elem = schedule[(team, i)]
-                output += self.str_game(team, dom_elem) + "\n"
+                dom_elem = schedule[(t, i)]
+                output += self.str_game(t, dom_elem) + "\n"
         return output
                 
     def str_game(self, team, dom_elem):
@@ -131,6 +169,8 @@ class Scheduler(object):
         return "{},{},{}".format(self.data.i2d[dateindex], team.name if home else opponent.name, opponent.name if home else team.name)
 
 if __name__ == '__main__':
+    f = open('output.txt', 'w')
+    sys.stdout = f
     sched = Scheduler(2015)
     today = dg.datetime.datetime.today()
     new_sched = sched.create_schedule()
@@ -138,4 +178,4 @@ if __name__ == '__main__':
     elapsed = fin - today
     elapsed = elapsed.total_seconds()
     print elapsed/60.
-    #output new sched to file
+    f.close()
